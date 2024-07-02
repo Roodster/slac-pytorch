e48e97aa25e79edcaf1b800f629a790c4775e9f5
Rody Haket <rhaket@student.tudelft.nl>
2024-07-02 09:06:07 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     keywords='slac disentanglement drl',
     packages=find_packages(),
     install_requires=[
+        'dmc2gym',
         'torch',
         'tensorboard',
         'gymnasium',
